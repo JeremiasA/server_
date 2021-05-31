35c92ca473e86d3488550d6fb5a90e624a62a5c1
Federico Lopez Carrizo <flopezcarrizo@gmail.com>
2021-05-31 00:24:07 +0000

    [OT36-33] Añadir lógica de JWT

--- config/config.js
+++ config/config.js
@@ -22,5 +22,6 @@ module.exports = {
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "dialect": "mysql"
-    }
+    },
+    "SECRET_TOKEN": process.env.SECRET_TOKEN || "tokensalkemy2021"
 }
